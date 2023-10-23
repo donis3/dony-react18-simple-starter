9b8c81e08f6df30b94c0620eccfaa02e7116278d
Deniz <donis3@gmail.com>
2023-10-23 15:26:26 +0300

    added base for gh pages in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import open, { apps } from "open";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "/dony-react18-simple-starter/",
 	plugins: [react()],
 	server: {
 		open: await open("http://localhost:3000/", {
